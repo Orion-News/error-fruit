a41f7c1948e0c622c22aac844a04291ead30fc4a
Orion-News <geeh-eb@Hotmail.com>
2022-04-12 14:02:57 -0300

    feat: ensure SignUpController return an error if no email or password is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,22 @@
 export class SignUpController {
   handle (httpRequest: any) {
-    return {
+    if (!httpRequest.body.name) {
+      return {
         statusCode: 400,
         body: new Error('Missing param: name')
+      }
+    }
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: email')
+      }
+    }
+    if (!httpRequest.body.password) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: password')
+      }
     }
   }
 }
