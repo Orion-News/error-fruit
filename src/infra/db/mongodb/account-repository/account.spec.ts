74486c25ba34065ce165ee1ad78dfa836b8dce76
Orion-News <geeh-eb@Hotmail.com>
2022-04-18 10:41:35 -0300

    chore: commit test return an account on success

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,26 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+
+describe('Account Mongo Repository', () => {
+    beforeAll(async () => {
+      await MongoHelper.connect(global.__MONGO_URI__)
+    })
+
+    afterAll(async () => {
+        await MongoHelper.disconnect()
+    })
+
+    test('Should return an account pm success', () => {
+        const sut = new AccountMongoRepository()
+        const account = await sut.add({
+            name: 'any_name',
+            email: 'any_mail@mail.com',
+            password: 'any_password'
+        })
+
+        expect(account).toBeTruthy()
+        expect(account.id).toBeTruthy()
+        expect(account.name).toBe('any_name')
+        expect(account.name).toBe('any_mail@mail.com')
+        expect(account.name).toBe('any_password')
+    })
+})
