faa01ebcbfc64b7aee0fec50ea05d192797e1bb8
Orion-News <geeh-eb@Hotmail.com>
2022-04-18 15:33:00 -0300

    refactor: connect to mongodb before integration tests

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,7 +1,22 @@
 import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
 
 describe('SignUp Routes', () => {
+  
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+      await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+      const accountCollection = MongoHelper.getCollection('accounts')
+      await accountCollection.deleteMany({})
+  })
+
   test('Should return an account on sucess', async () => {
     await request(app)
       .post('/api/signup')
@@ -10,6 +25,7 @@ describe('SignUp Routes', () => {
         email: 'geverson@mail.com',
         password: '123',
         passwordConfirmation: '123'
-      }).expect(200)
+      })
+      .expect(200)
   })
 })
